7c31758899af7a9aec169bd34d216594fa7f8d76
Vasileios-Pistikopoulos <BasilisPistikopoulos@gmail.com>
2025-03-18 18:08:21 +0200

    init commit

---
+++ src/main/java/__5281/swdeproj/SwdeprojApplication.java
@@ -0,0 +1,13 @@
+package __5281.swdeproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwdeprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwdeprojApplication.class, args);
+	}
+
+}
